fd50d51862be691fc6cd8dc906e4a298d5499d0e
gomgoguma <kebhse@daum.net>
2024-01-17 22:35:18 +0900

    update swagger config

--- src/main/java/com/oha/posting/config/SwaggerConfig.java
+++ src/main/java/com/oha/posting/config/SwaggerConfig.java
@@ -1,17 +1,35 @@
 package com.oha.posting.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
-@OpenAPIDefinition(
-        info = @Info(title = "OHA",
-                description = "API명세",
-                version = "0.0.1"))
-@RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .version("v1.0.0")
+                .title("OHA Posting API")
+                .description("");
+
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 }
--- src/main/java/com/oha/posting/controller/HomeController.java
+++ src/main/java/com/oha/posting/controller/HomeController.java
@@ -3,8 +3,8 @@ package com.oha.posting.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name="업로드", description = "업로드 API")
+@Tag(name="홈", description = "홈 API")
 @RestController
-public class PostController {
+public class HomeController {
 
 }
