b687c8a69b3e32a72d5ddf4a28190d355bf3c254
gomgoguma <kebhse@daum.net>
2024-01-23 18:38:49 +0900

    update config

--- src/main/java/com/oha/posting/config/response/ResponseObject.java
+++ src/main/java/com/oha/posting/config/response/ResponseObject.java
@@ -1,11 +1,15 @@
 package com.oha.posting.config.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseObject<T> {
 
     @Schema(description = "상태코드", example = "200")
@@ -14,4 +18,20 @@ public class ResponseObject<T> {
     private String message;
     @Schema(description = "데이터")
     private T data;
+
+    public ResponseObject(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public void setResponse(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public void setResponse(int statusCode, String message, T data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
 }
--- src/main/java/com/oha/posting/config/swagger/SwaggerConfig.java
+++ src/main/java/com/oha/posting/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.oha.posting.config;
+package com.oha.posting.config.swagger;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
