9991920f086af2e9bec21e9b00fe877ae7baae4a
gomgoguma <kebhse@daum.net>
2024-02-13 17:53:19 +0900

    Update ObjectMapperConfig
    - allow missing attributes

--- src/main/java/com/oha/posting/config/mapper/ObjectMapperConfig.java
+++ src/main/java/com/oha/posting/config/mapper/ObjectMapperConfig.java
@@ -1,5 +1,6 @@
 package com.oha.posting.config.mapper;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,8 @@ public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper(){
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); // 매핑 대상에 없는 속성 무시
+        return objectMapper;
     }
 }
